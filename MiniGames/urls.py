0d55a07be84581b2e1699b67196b8e459a30d776
Olesia <melnuchun@icloud.com>
2020-03-16 23:02:09 +0100

    app Main: Homepage

--- MiniGames/urls.py
+++ MiniGames/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hangman/', include('Hangman.urls')),
+    path(r'', include('Main.urls')),
 ]
